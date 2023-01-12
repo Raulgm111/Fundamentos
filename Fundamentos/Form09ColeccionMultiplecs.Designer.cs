61c72411820115ce29a9718b7a503d817a898e95
Raulgm111~ <raul.garciamoratinos@outlook.com~>
2023-01-12 12:58:03 +0100

    commit

---
+++ Fundamentos/Form09ColeccionMultiplecs.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Fundamentos
+{
+    partial class Form09ColeccionMultiplecs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblItem = new System.Windows.Forms.Label();
+            this.lblindice = new System.Windows.Forms.Label();
+            this.btnBorrarTodo = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.txtNuevoElemnto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listElementos = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSeleccionados = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Location = new System.Drawing.Point(115, 217);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(44, 15);
+            this.lblItem.TabIndex = 17;
+            this.lblItem.Text = "lblItem";
+            // 
+            // lblindice
+            // 
+            this.lblindice.AutoSize = true;
+            this.lblindice.Location = new System.Drawing.Point(115, 185);
+            this.lblindice.Name = "lblindice";
+            this.lblindice.Size = new System.Drawing.Size(52, 15);
+            this.lblindice.TabIndex = 16;
+            this.lblindice.Text = "lblindice";
+            // 
+            // btnBorrarTodo
+            // 
+            this.btnBorrarTodo.Location = new System.Drawing.Point(508, 244);
+            this.btnBorrarTodo.Name = "btnBorrarTodo";
+            this.btnBorrarTodo.Size = new System.Drawing.Size(145, 30);
+            this.btnBorrarTodo.TabIndex = 15;
+            this.btnBorrarTodo.Text = "Borrar Todo";
+            this.btnBorrarTodo.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(508, 157);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(145, 30);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(508, 111);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(145, 30);
+            this.btnInsertar.TabIndex = 13;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // txtNuevoElemnto
+            // 
+            this.txtNuevoElemnto.Location = new System.Drawing.Point(508, 69);
+            this.txtNuevoElemnto.Name = "txtNuevoElemnto";
+            this.txtNuevoElemnto.Size = new System.Drawing.Size(145, 23);
+            this.txtNuevoElemnto.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(508, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nuevo Elemento";
+            // 
+            // listElementos
+            // 
+            this.listElementos.FormattingEnabled = true;
+            this.listElementos.ItemHeight = 15;
+            this.listElementos.Location = new System.Drawing.Point(106, 69);
+            this.listElementos.Name = "listElementos";
+            this.listElementos.Size = new System.Drawing.Size(120, 94);
+            this.listElementos.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Elementos";
+            // 
+            // btnSeleccionados
+            // 
+            this.btnSeleccionados.Location = new System.Drawing.Point(508, 202);
+            this.btnSeleccionados.Name = "btnSeleccionados";
+            this.btnSeleccionados.Size = new System.Drawing.Size(145, 30);
+            this.btnSeleccionados.TabIndex = 18;
+            this.btnSeleccionados.Text = "Seleccionados";
+            this.btnSeleccionados.UseVisualStyleBackColor = true;
+            // 
+            // Form09ColeccionMultiplecs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSeleccionados);
+            this.Controls.Add(this.lblItem);
+            this.Controls.Add(this.lblindice);
+            this.Controls.Add(this.btnBorrarTodo);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtNuevoElemnto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listElementos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form09ColeccionMultiplecs";
+            this.Text = "Form09ColeccionMultiplecs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblItem;
+        private Label lblindice;
+        private Button btnBorrarTodo;
+        private Button btnEliminar;
+        private Button btnInsertar;
+        private TextBox txtNuevoElemnto;
+        private Label label2;
+        private ListBox listElementos;
+        private Label label1;
+        private Button btnSeleccionados;
+    }
+}
---
+++ Fundamentos/Form09ColeccionMultiplecs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos
+{
+    public partial class Form09ColeccionMultiplecs : Form
+    {
+        public Form09ColeccionMultiplecs()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            string elemento = this.txtNuevoElemnto.Text;
+            this.listElementos.Items.Add(elemento);
+            this.txtNuevoElemnto.Focus();
+            this.txtNuevoElemnto.SelectAll();
+        }
+    }
+}
