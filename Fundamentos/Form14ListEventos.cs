5330df9a088f59825695f6735c12cd2bb3465547
Raulgm111~ <raul.garciamoratinos@outlook.com~>
2023-01-16 11:09:59 +0100

    commit

---
+++ Fundamentos/Form14ListEventos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos
+{
+    public partial class Form14ListEventos : Form
+    {
+        //DECLARAMOS LA COLECCION A NIVEL DE CLASE 
+        //PARA UTILIZARLA EN OTROS EVENTOS
+        List<Button> botones;
+
+        int contador=0;
+        public Form14ListEventos()
+        {
+            InitializeComponent();
+            this.botones = new List<Button>();
+            //VAMOS A RELLENAR LA COLECCION CON TODOS LOS BOTONES DEL DIBUJO
+            //this.botones.Add(this.button1);
+            //DENTRO DE LA CLASE FORM TENEMOS UNA COLECCION LLAMADA controls 
+            //QUE CONTIENE TODOS LOS CONTROLES DEL FORM
+            //VAMOS A RECORRES TODOS LOS CONTROLES Y AÑADIRLOS A NUESTRA COLECCION DE FORMA DINAMICA
+            foreach(Control control in this.Controls)
+            {
+                //DEBEMSO PREGUNAR POR LOS CONTROLES BUTTON
+                if(control is Button)
+                {
+                    this.botones.Add((Button)control);
+                }
+            }
+            //AQUI MANEJAMOS NUESTRA COLECCION PERSONALIZADA
+            //ASOCIAMOS CADA BOTON DE LA COLECCION A UN METODO EVENTO
+            foreach(Button boton in this.botones)
+            {
+                boton.Click += BotonPulsado;
+            }
+        }
+
+        private void BotonPulsado(object? sender, EventArgs e)
+        {
+            this.contador++;
+            this.txtMensaje.Text = "Contador: " + this.contador;
+            //NECESITO ACCEDER AL BOTON, CUANDO PULSEMAOS SOBRE EL BOTON CAMBIAMOS SU COLOR
+            Button boton = (Button)sender;
+            boton.BackColor = Color.OrangeRed;
+            boton.Text = this.contador.ToString();
+        }
+    }
+}
---
+++ Fundamentos/Form15SumarBotones.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos
+{
+    public partial class Form15SumarBotones : Form
+    {
+        List<Button> botones;
+        //NECESITAMOS UNA VARIABLE PARA ALMACENAR EL 
+        //VALOR ACTUAL DE LA SUMA A NIVEL DE CLASE
+        int suma;
+        public Form15SumarBotones()
+        {
+            InitializeComponent();
+            this.botones = new List<Button>();
+            this.suma = 0;
+            //RECORREMOS TODA LA COLECCION CONTROL Y ACCEDEMOS A CADA UNO DE LOS BOTONES
+            foreach(Control control in this.panelBotones.Controls)
+            {
+                if(control is Button)
+                {
+                    this.botones.Add((Button)control);
+                }
+            }
+            //RECORREMOS LOS CONTROLES Y ASIGANMOS NUMEROS RANDOM
+            //EN CADA UNo
+            Random random = new Random();
+            foreach(Button boton in this.botones)
+            {
+                int num=random.Next(1,200);
+                boton.Text=num.ToString();
+                boton.Click += SumarNumero;
+            }
+        }
+
+        private void SumarNumero(object? sender, EventArgs e)
+        {
+            //RECU`PERAMOS EL NUMERO DEL BOTON PULSADO
+            Button boton = (Button)sender;
+            int numero = int.Parse(boton.Text);
+            this.suma += numero;
+            this.txtSuma.Text=this.suma.ToString();
+            boton.BackColor=Color.CornflowerBlue;
+        }
+
+        private void btnReinicarApp_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            this.suma = 0;
+            this.txtSuma.Text = "0";
+            foreach (Button boton in this.botones)
+            {
+                int num = random.Next(1, 200);
+                boton.Text = num.ToString();
+                boton.Click += SumarNumero;
+                boton.BackColor=Color.FromKnownColor(KnownColor.White);
+            }
+        }
+    }
+}
