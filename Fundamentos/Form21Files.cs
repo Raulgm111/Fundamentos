bb5cdd7e8dedbef55f62c1cae3f6eed9ef57c815
Raulgm111~ <raul.garciamoratinos@outlook.com~>
2023-01-23 10:08:50 +0100

    commit

--- Fundamentos/Form21Files.cs
+++ Fundamentos/Form21Files.cs
@@ -7,15 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
+using ProyectoClasess;
+using ProyectoClasess.Helpers;
 
 namespace Fundamentos
 {
     public partial class Form21Files : Form
     {
+        HelperFiles helper;
         public Form21Files()
         {
             InitializeComponent();
+            this.helper=new HelperFiles();
         }
 
         private async void btnReadFile_Click(object sender, EventArgs e)
@@ -25,22 +28,11 @@ namespace Fundamentos
             if(ofd.ShowDialog() == DialogResult.OK)
             {
                 string path = ofd.FileName;
-                //EL OBJETO FILEINFO ES UN OBJETO PARA
-                //ACCEDER A LA INFROMACION DE UN FICHERO
-                FileInfo file = new FileInfo(path);
-                //PARA LEER VAMOS A UTILIZAR using
-                //PARA ASEGURARNOS QUE DENTRO DEL CODIGO
-                //EL OBJETO SIEMPRE ESTARA ACCESIBLE
-                using(TextReader reader = file.OpenText())
-                {
-                    //DENTRO DE ESTE CODIGO DEBEMOS UTILIZAR READER
-                    //Y DESPUES QUEDARA DESTRUIDO
-                    string contenido= await reader.ReadToEndAsync();
-                    reader.Close();
-                    this.txtContenido.Text=contenido;
+                string contenido = await this.helper.ReadFileAsync(path);
+                this.txtContenido.Text=contenido;
                     //DIBUJAMOS LOS DATOS EN UN LISTBOX
                     this.SetStringNombres(contenido);
-                }
+                
             }
         }
 
@@ -50,17 +42,8 @@ namespace Fundamentos
             if(save.ShowDialog() == DialogResult.OK)
             {
                 string path=save.FileName;
-                FileInfo file=new FileInfo(path);
-                using(TextWriter writer = file.CreateText())
-                {
-                    //EL CONTENIDO QUE DESEAMOS ESCRIBIR ESTA EN EL LISTBOX
-                    string contenido=this.GetStringNombres();
-                    await writer.WriteAsync(contenido);
-                    //SIEMPRE QUE TRABAJEMOS CON FILES, AL ESCRIBIR
-                    //DEBEMOS TERMINAR CON EL METODO FLUSH DESPUES DE ESCRIBIR
-                    await writer.FlushAsync();
-                    writer.Close();
-                }
+                string contenido = this.GetStringNombres();
+                await this.helper.WriteFileAsync(path, contenido);
                 MessageBox.Show("Datos guardados");
             }
         }
---
+++ ProyectoClasess/Helpers/HelperFiles.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace ProyectoClasess.Helpers
+{
+    public class HelperFiles
+    {
+        //NECESITAMOS UN METODO PARA LEER FICHEROS
+        //ESTE METODO DEVUELVE EL STRING DEL FILE
+        //EN LAS CLASES LOS METODOS ASINCRONOS
+        //UTILIZAN LA CLASE task PARA SU DECLARACION
+        //SI ES UN void SIMPLEMETE LA PALABRA task
+        //SI ES UN return, SE UTILIZA Task<Tipo>
+        public async Task<string> ReadFileAsync(string path)
+        {
+            FileInfo file = new FileInfo(path);
+            //PARA LEER VAMOS A UTILIZAR using
+            //PARA ASEGURARNOS QUE DENTRO DEL CODIGO
+            //EL OBJETO SIEMPRE ESTARA ACCESIBLE
+            string contenido = "";
+            using (TextReader reader = file.OpenText())
+            {
+                //DENTRO DE ESTE CODIGO DEBEMOS UTILIZAR READER
+                //Y DESPUES QUEDARA DESTRUIDO
+                contenido = await reader.ReadToEndAsync();
+                reader.Close();
+            }
+            return contenido;
+        }
+
+        //METODO PARA ESCRIBIR UN FILE
+        //ESTE METODO ES DE ACCION (void)
+        //NO DEVUELVE NADA
+        public async Task WriteFileAsync(string path,string data)
+        {
+            FileInfo file = new FileInfo(path);
+            using (TextWriter writer = file.CreateText())
+            {
+                await writer.WriteAsync(data);
+                await writer.FlushAsync();
+                writer.Close();
+            }
+        }
+
+    }
+}
