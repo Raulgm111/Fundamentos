6021c665f8eb7535fadeafea2f15fcfa06b18fb6
Raulgm111~ <raul.garciamoratinos@outlook.com~>
2023-01-23 09:44:56 +0100

    commit

---
+++ Fundamentos/Form21Files.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Fundamentos
+{
+    partial class Form21Files
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.btnWriteFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listNombres = new System.Windows.Forms.ListBox();
+            this.txtContenido = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Contenido File";
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(128, 227);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnReadFile.TabIndex = 2;
+            this.btnReadFile.Text = "Read File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // btnWriteFile
+            // 
+            this.btnWriteFile.Location = new System.Drawing.Point(128, 256);
+            this.btnWriteFile.Name = "btnWriteFile";
+            this.btnWriteFile.Size = new System.Drawing.Size(75, 23);
+            this.btnWriteFile.TabIndex = 3;
+            this.btnWriteFile.Text = "Write File";
+            this.btnWriteFile.UseVisualStyleBackColor = true;
+            this.btnWriteFile.Click += new System.EventHandler(this.btnWriteFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(362, 48);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 23);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(480, 47);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(362, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nombres";
+            // 
+            // listNombres
+            // 
+            this.listNombres.FormattingEnabled = true;
+            this.listNombres.ItemHeight = 15;
+            this.listNombres.Location = new System.Drawing.Point(362, 117);
+            this.listNombres.Name = "listNombres";
+            this.listNombres.Size = new System.Drawing.Size(215, 154);
+            this.listNombres.TabIndex = 8;
+            // 
+            // txtContenido
+            // 
+            this.txtContenido.Location = new System.Drawing.Point(69, 48);
+            this.txtContenido.Name = "txtContenido";
+            this.txtContenido.Size = new System.Drawing.Size(227, 164);
+            this.txtContenido.TabIndex = 9;
+            this.txtContenido.Text = "";
+            // 
+            // Form21Files
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtContenido);
+            this.Controls.Add(this.listNombres);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnWriteFile);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form21Files";
+            this.Text = "Form21Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnReadFile;
+        private Button btnWriteFile;
+        private Label label2;
+        private TextBox txtNombre;
+        private Button btnAgregar;
+        private Label label3;
+        private ListBox listNombres;
+        private RichTextBox txtContenido;
+    }
+}
---
+++ Fundamentos/Form21Files.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Fundamentos
+{
+    public partial class Form21Files : Form
+    {
+        public Form21Files()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnReadFile_Click(object sender, EventArgs e)
+        {
+            //OBJETO PARA ABRIR FILES
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                string path = ofd.FileName;
+                //EL OBJETO FILEINFO ES UN OBJETO PARA
+                //ACCEDER A LA INFROMACION DE UN FICHERO
+                FileInfo file = new FileInfo(path);
+                //PARA LEER VAMOS A UTILIZAR using
+                //PARA ASEGURARNOS QUE DENTRO DEL CODIGO
+                //EL OBJETO SIEMPRE ESTARA ACCESIBLE
+                using(TextReader reader = file.OpenText())
+                {
+                    //DENTRO DE ESTE CODIGO DEBEMOS UTILIZAR READER
+                    //Y DESPUES QUEDARA DESTRUIDO
+                    string contenido= await reader.ReadToEndAsync();
+                    reader.Close();
+                    this.txtContenido.Text=contenido;
+                    //DIBUJAMOS LOS DATOS EN UN LISTBOX
+                    this.SetStringNombres(contenido);
+                }
+            }
+        }
+
+        private async void btnWriteFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            if(save.ShowDialog() == DialogResult.OK)
+            {
+                string path=save.FileName;
+                FileInfo file=new FileInfo(path);
+                using(TextWriter writer = file.CreateText())
+                {
+                    //EL CONTENIDO QUE DESEAMOS ESCRIBIR ESTA EN EL LISTBOX
+                    string contenido=this.GetStringNombres();
+                    await writer.WriteAsync(contenido);
+                    //SIEMPRE QUE TRABAJEMOS CON FILES, AL ESCRIBIR
+                    //DEBEMOS TERMINAR CON EL METODO FLUSH DESPUES DE ESCRIBIR
+                    await writer.FlushAsync();
+                    writer.Close();
+                }
+                MessageBox.Show("Datos guardados");
+            }
+        }
+
+        //METODO PARA RCUPERAR TODOS LOS NOMBRES
+        //DE LA LISTA EN FORMATO SEPARADOS POR COMAS
+        public string GetStringNombres()
+        {
+            string datos = "";
+            foreach(string nombre in this.listNombres.Items)
+            {
+                datos += nombre + ",";
+            }
+            datos = datos.Trim(',');
+            return datos;
+        }
+
+        //METODO PARA ESCRIBIR LOS NOMBRES QUE
+        //TENGAMOS EN UN STRING EN EL LISTBOX
+        public void SetStringNombres(string data)
+        {
+            string[] nombres = data.Split(',');
+            this.listNombres.Items.Clear();
+            foreach(string name in nombres)
+            {
+                this.listNombres.Items.Add(name);
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre = this.txtNombre.Text;
+            this.listNombres.Items.Add(nombre);
+            this.txtNombre.SelectAll();
+            this.txtNombre.Focus();
+        }
+    }
+}
