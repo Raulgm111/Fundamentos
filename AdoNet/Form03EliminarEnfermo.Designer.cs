3c4f3bcb3bfb49c9dcdf7a457dfcb9774abbb872
Raulgm111~ <raul.garciamoratinos@outlook.com~>
2023-01-26 12:49:43 +0100

    commit

---
+++ AdoNet/Form03EliminarEnfermo.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AdoNet
+{
+    partial class Form03EliminarEnfermo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInscripcion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listEnfermos = new System.Windows.Forms.ListBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inscripcion";
+            // 
+            // txtInscripcion
+            // 
+            this.txtInscripcion.Location = new System.Drawing.Point(62, 46);
+            this.txtInscripcion.Name = "txtInscripcion";
+            this.txtInscripcion.Size = new System.Drawing.Size(100, 23);
+            this.txtInscripcion.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enfermos";
+            // 
+            // listEnfermos
+            // 
+            this.listEnfermos.FormattingEnabled = true;
+            this.listEnfermos.ItemHeight = 15;
+            this.listEnfermos.Location = new System.Drawing.Point(295, 46);
+            this.listEnfermos.Name = "listEnfermos";
+            this.listEnfermos.Size = new System.Drawing.Size(258, 199);
+            this.listEnfermos.TabIndex = 3;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(112, 111);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(96, 35);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form03EliminarEnfermo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.listEnfermos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInscripcion);
+            this.Controls.Add(this.label1);
+            this.Name = "Form03EliminarEnfermo";
+            this.Text = "Form03EliminarEnfermo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtInscripcion;
+        private Label label2;
+        private ListBox listEnfermos;
+        private Button btnEliminar;
+    }
+}
---
+++ AdoNet/Form03EliminarEnfermo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNet
+{
+    public partial class Form03EliminarEnfermo : Form
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+        public Form03EliminarEnfermo()
+        {
+            InitializeComponent();
+            string connectionString =
+                "Data Source=LOCALHOST\\DESARROLLO;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Password=MCSD2023";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+            this.LoadEnfermos();
+        }
+        private void LoadEnfermos()
+        {
+            string sqp = "SELECT * FROM ENFERMO";
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sqp;
+            this.cn.Open();
+            this.reader=this.com.ExecuteReader();
+            this.listEnfermos.Items.Clear();
+            while (this.reader.Read())
+            {
+                string inscripcion = this.reader["INSCRIPCION"].ToString();
+                string apellido = this.reader["APELLIDO"].ToString();
+                this.listEnfermos.Items.Add(inscripcion + " - " + apellido);
+            }
+            this.reader.Close();
+            this.cn.Close();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
