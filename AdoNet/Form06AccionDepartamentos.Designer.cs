4f8a939264f9cf64a1d7c47406f9709014bfec5f
Raulgm111~ <raul.garciamoratinos@outlook.com~>
2023-01-27 12:42:46 +0100

    commit

---
+++ AdoNet/Form06AccionDepartamentos.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace AdoNet
+{
+    partial class Form06AccionDepartamentos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listDepartamentos = new System.Windows.Forms.ListBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtLocalidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Departamentos";
+            // 
+            // listDepartamentos
+            // 
+            this.listDepartamentos.FormattingEnabled = true;
+            this.listDepartamentos.ItemHeight = 15;
+            this.listDepartamentos.Location = new System.Drawing.Point(98, 58);
+            this.listDepartamentos.Name = "listDepartamentos";
+            this.listDepartamentos.Size = new System.Drawing.Size(199, 184);
+            this.listDepartamentos.TabIndex = 1;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(394, 58);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(123, 37);
+            this.btnInsertar.TabIndex = 2;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(394, 101);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(123, 37);
+            this.btnModificar.TabIndex = 3;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(394, 144);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(123, 37);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(394, 203);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(123, 23);
+            this.txtId.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(394, 244);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(123, 23);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtLocalidad
+            // 
+            this.txtLocalidad.Location = new System.Drawing.Point(394, 286);
+            this.txtLocalidad.Name = "txtLocalidad";
+            this.txtLocalidad.Size = new System.Drawing.Size(123, 23);
+            this.txtLocalidad.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(394, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Localidad";
+            // 
+            // Form06AccionDepartamentos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLocalidad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.listDepartamentos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form06AccionDepartamentos";
+            this.Text = "Form06AccionDepartamentos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listDepartamentos;
+        private Button btnInsertar;
+        private Button btnModificar;
+        private Button btnEliminar;
+        private TextBox txtId;
+        private TextBox txtNombre;
+        private TextBox txtLocalidad;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
--- AdoNet/Repositories/RepositoryDepartamentos.cs
+++ AdoNet/Repositories/RepositoryDepartamentos.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using AdoNet.Models;
+using System.Data;
 
 namespace AdoNet.Repositories
 {
@@ -21,17 +23,72 @@ namespace AdoNet.Repositories
             this.com = new SqlCommand();
             this.com.Connection = this.cn;
         }
+        public List<Departamento> GetDepartamentos()
+        {
+            string sql = "SELECT * FROM DEPT";
+            List<Departamento> departamentos = new List<Departamento>();
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText= sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            while (this.reader.Read())
+            {
+                int id = int.Parse(this.reader["DEPT_NO"].ToString());
+                string nombre = this.reader["DNOMBRE"].ToString();
+                string localidad = this.reader["LOC"].ToString();
+                Departamento dept=new Departamento();
+                dept.IdDepartamento = id;
+                dept.Nombre = nombre;
+                dept.Localidad = localidad;
+                departamentos.Add(dept);
+            }
+            this.reader.Close();
+            this.cn.Close();
+            return departamentos;
+        }
+
+        public int DeleteDepartamento(int id)
+        {
+            string sql = "DELETE FROM DEPT WHERE DEPT_NO=@NUM";
+            SqlParameter pamnum = new SqlParameter("@NUM", id);
+            this.com.Parameters.Add(pamnum);
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            int eliminados = this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+            return eliminados;
+        }
+
+        public int UpdateDepartamentos(int id, string nombre, string localidad)
+        {
+            string sql = "UPDATE DEPT SET DNOMBRE=@NOM,LOC=@LOC WHERE DEPT_NO=@NUM";
+            SqlParameter pamnum = new SqlParameter("@NUM", id);
+            SqlParameter pamnom = new SqlParameter("@NOM", nombre);
+            SqlParameter pamloc = new SqlParameter("@LOC", localidad);
+            this.com.Parameters.Add(pamnum);
+            this.com.Parameters.Add(pamnom);
+            this.com.Parameters.Add(pamloc);
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            int modificados = this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+            return modificados;
+        }
 
         public int InsertDepartamentos(int id,string nombre,string localidad)
         {
             string sql = "INSERT INTO DEPT VALUES (@NUM,@NOM,@LOC)";
             SqlParameter pamnum = new SqlParameter("@NUM", id);
-            SqlParameter pamnom = new SqlParameter("@NOM", id);
-            SqlParameter pamloc = new SqlParameter("@LOC", id);
+            SqlParameter pamnom = new SqlParameter("@NOM", nombre);
+            SqlParameter pamloc = new SqlParameter("@LOC", localidad);
             this.com.Parameters.Add(pamnum);
             this.com.Parameters.Add(pamnom);
             this.com.Parameters.Add(pamloc);
-            this.com.CommandType=System.Data.CommandType.Text;
+            this.com.CommandType=CommandType.Text;
             this.com.CommandText=sql;
             this.cn.Open();
             int insertados = this.com.ExecuteNonQuery();
