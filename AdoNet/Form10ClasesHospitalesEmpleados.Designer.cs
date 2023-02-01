778f16da35e20c541ab9f8cb96461c4643b14085
Raulgm111~ <raul.garciamoratinos@outlook.com~>
2023-02-01 10:08:33 +0100

    commit

---
+++ AdoNet/Form10ClasesHospitalesEmpleados.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace AdoNet
+{
+    partial class Form10ClasesHospitalesEmpleados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPersonas = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSuma = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listEmpleados = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboHospitales = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPersonas
+            // 
+            this.txtPersonas.Location = new System.Drawing.Point(82, 229);
+            this.txtPersonas.Name = "txtPersonas";
+            this.txtPersonas.Size = new System.Drawing.Size(121, 23);
+            this.txtPersonas.TabIndex = 30;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 15);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Personas";
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(82, 185);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.Size = new System.Drawing.Size(121, 23);
+            this.txtMedia.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 15);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Media";
+            // 
+            // txtSuma
+            // 
+            this.txtSuma.Location = new System.Drawing.Point(82, 140);
+            this.txtSuma.Name = "txtSuma";
+            this.txtSuma.Size = new System.Drawing.Size(121, 23);
+            this.txtSuma.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Suma";
+            // 
+            // listEmpleados
+            // 
+            this.listEmpleados.FormattingEnabled = true;
+            this.listEmpleados.ItemHeight = 15;
+            this.listEmpleados.Location = new System.Drawing.Point(319, 87);
+            this.listEmpleados.Name = "listEmpleados";
+            this.listEmpleados.Size = new System.Drawing.Size(394, 139);
+            this.listEmpleados.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Empleados";
+            // 
+            // comboHospitales
+            // 
+            this.comboHospitales.FormattingEnabled = true;
+            this.comboHospitales.Location = new System.Drawing.Point(82, 87);
+            this.comboHospitales.Name = "comboHospitales";
+            this.comboHospitales.Size = new System.Drawing.Size(200, 23);
+            this.comboHospitales.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Hospitales Clases";
+            // 
+            // Form10ClasesHospitalesEmpleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPersonas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSuma);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listEmpleados);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboHospitales);
+            this.Controls.Add(this.label1);
+            this.Name = "Form10ClasesHospitalesEmpleados";
+            this.Text = "Form10ClasesHospitalesEmpleados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtPersonas;
+        private Label label5;
+        private TextBox txtMedia;
+        private Label label4;
+        private TextBox txtSuma;
+        private Label label3;
+        private ListBox listEmpleados;
+        private Label label2;
+        private ComboBox comboHospitales;
+        private Label label1;
+    }
+}
--- AdoNet/Repositories/RepositoryHospital.cs
+++ AdoNet/Repositories/RepositoryHospital.cs
@@ -1,98 +1,50 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics.Metrics;
-using System.Drawing;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using static System.ComponentModel.Design.ObjectSelectorEditor;
+using System.Data;
+using AdoNet.Models;
 
-namespace AdoNet
+namespace AdoNet.Repositories
 {
-    #region PROCEDURES Y VIEWS
-//    CREATE PROCEDURE SP_HOSPITALES
-//AS
-
-//    SELECT* FROM HOSPITAL
-//GO
-
-//    ALTER PROCEDURE SP_EMPLEADOS_HOSPITAL
-//(@NOMBRE NVARCHAR(50),
-//@SUMA INT OUT,
-//@MEDIA INT OUT,
-//@PERSONAS INT OUT)
-//AS
-//    DECLARE @IDHOSPITAL INT
-
-    //    SELECT @IDHOSPITAL = HOSPITAL_COD FROM HOSPITAL
-
-    //    WHERE NOMBRE = @NOMBRE
-
-
-    //    SELECT plantilla.APELLIDO, PLANTILLA.SALARIO FROM PLANTILLA WHERE PLANTILLA.HOSPITAL_COD= @IDHOSPITAL
-
-    //    UNION
-    //    SELECT DOCTOR.APELLIDO, DOCTOR.SALARIO FROM DOCTOR WHERE DOCTOR.HOSPITAL_COD= @IDHOSPITAL
-
-
-    //    SELECT @SUMA = SUM(QUERY.SALARIO), @MEDIA = AVG(QUERY.SALARIO), @PERSONAS = COUNT(QUERY.APELLIDO)
-
-    // FROM
-
-    // (SELECT D.APELLIDO, D.SALARIO FROM DOCTOR D
-
-    // WHERE D.HOSPITAL_COD= @IDHOSPITAL
-
-    // UNION
-    // SELECT P.APELLIDO, P.SALARIO FROM PLANTILLA P
-
-    // WHERE P.HOSPITAL_COD= @IDHOSPITAL)
-
-    // QUERY
-    //GO
-    #endregion
-    public partial class Form10HospitalEmpleados : Form
+    public class RepositoryHospital
     {
         SqlConnection cn;
         SqlCommand com;
         SqlDataReader reader;
-        public Form10HospitalEmpleados()
+        
+        public RepositoryHospital()
         {
-            InitializeComponent();
             string connectionString =
                 "Data Source=LOCALHOST\\DESARROLLO;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Password=MCSD2023";
             this.cn = new SqlConnection(connectionString);
             this.com = new SqlCommand();
             this.com.Connection = cn;
-            this.LoadHospitales();
-
         }
 
-        private void LoadHospitales()
+        //NECESITMAOS UN METODO PARA DEVOLVER TODOS LOS HOSPITALES
+        public List<string> GetHospitales()
         {
             this.com.CommandType = CommandType.StoredProcedure;
             this.com.CommandText = "SP_HOSPITALES";
+            List<string> hospitales = new List<string>();
             this.cn.Open();
             this.reader = this.com.ExecuteReader();
-            this.comboHospitales.Items.Clear();
+            //hospitales.Clear();
             while (this.reader.Read())
             {
                 string nombre = this.reader["NOMBRE"].ToString();
-                this.comboHospitales.Items.Add(nombre);
+                hospitales.Add(nombre);
             }
             this.reader.Close();
             this.cn.Close();
+            return hospitales;
         }
 
-        private void comboHospitales_SelectedIndexChanged(object sender, EventArgs e)
+        public DatosHospital GetDatosHospital(string nombre)
         {
-            string nombre = this.comboHospitales.SelectedItem.ToString();
             SqlParameter paranombre = new SqlParameter("@NOMBRE", nombre);
             this.com.Parameters.Add(paranombre);
             SqlParameter pamsuma = new SqlParameter("@SUMA", 0);
@@ -104,26 +56,32 @@ namespace AdoNet
             SqlParameter pampersonas = new SqlParameter("@PERSONAS", 0);
             pampersonas.Direction = ParameterDirection.Output;
             this.com.Parameters.Add(pampersonas);
-
             this.com.CommandType = CommandType.StoredProcedure;
             this.com.CommandText = "SP_EMPLEADOS_HOSPITAL";
             this.cn.Open();
             this.reader = this.com.ExecuteReader();
-            this.listEmpleados.Items.Clear();
+            List<EmpleadoHospital>empleados = new List<EmpleadoHospital>();
             while (this.reader.Read())
             {
+                int idEmpleado = int.Parse(this.reader["APELLIDO"].ToString());
                 string apellido = this.reader["APELLIDO"].ToString();
-                string salario = this.reader["SALARIO"].ToString();
-                this.listEmpleados.Items.Add(apellido + " - " + salario);
+                int salario = int.Parse(this.reader["SALARIO"].ToString());
+                EmpleadoHospital empleado = new EmpleadoHospital();
+                empleado.IdEmpleado = idEmpleado;
+                empleado.Apellido = apellido;
+                empleado.Salario= salario;
+                empleados.Add(empleado);
             }
             this.reader.Close();
-
-            this.txtSuma.Text = pamsuma.Value.ToString();
-            this.txtMedia.Text = pammedia.Value.ToString();
-            this.txtPersonas.Text = pampersonas.Value.ToString();
+            DatosHospital datos=new DatosHospital();
+            //datos.empleados = empleados;
+            datos.SumaSalarial = int.Parse(pamsuma.Value.ToString());
+            datos.MediaSalarial = int.Parse(pammedia.Value.ToString());
+            datos.Personas = int.Parse(pampersonas.Value.ToString());
 
             this.cn.Close();
             this.com.Parameters.Clear();
+            return datos;
         }
     }
 }
